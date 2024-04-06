f5d97be02f1c2f7f41277629881ce8e4a151876a
sarman12 <sahaneearman601@gmail.com>
2024-04-06 11:01:26 +0530

    first commit

---
+++ weather.js
@@ -0,0 +1,53 @@
+const apikey="65f7b612e3b7902c807bca207f229519";
+const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+const search=document.querySelector(".search input");
+const btn=document.querySelector(".search btn");
+
+async function checkWeather(city){
+    const response=await fetch(apiurl+city+`&appid=65f7b612e3b7902c807bca207f229519`);
+    var data=await response.json();
+    console.log(data);
+    if(response.status == 404){
+        document.querySelector(".error").style.display="block";
+
+        document.querySelector(".weather").style.display="none";
+    }
+    else{
+                document.querySelector(".error").style.display="none";
+
+
+    }
+
+    document.querySelector(".city").innerHTML=data.name;
+    document.querySelector(".temp").innerHTML=data.main.temp+"°C";
+    document.querySelector(".humidity").innerHTML=data.main.humidity+"%";
+    document.querySelector(".wind").innerHTML=data.wind.speed+"km/h";
+    if(data.weather[0].main == "Clouds"){
+        document.querySelector(".weather-icon").src="images/clouds.png";
+    }
+    else if(data.weather[0].main == "Rain"){
+        document.querySelector(".weather-icon").src="images/Rain.png";
+    }
+    else if(data.weather[0].main == "Clear"){
+        document.querySelector(".weather-icon").src="images/clear.png";
+    }
+    else if(data.weather[0].main == "Mist"){
+        document.querySelector(".weather-icon").src="images/mist.png";
+    }
+    else if(data.weather[0].main == "Drizzle"){
+        document.querySelector(".weather-icon").src="images/Drizzle.png";
+    }
+    else if(data.weather[0].main == "Snow"){
+        document.querySelector(".weather-icon").src="images/snow.png";
+    }
+    else if(data.weather[0].main == "Smoke"){
+        document.querySelector(".weather-icon").src="images/smoke.png";
+    }
+    else if(data.weather[0].main == "Haze"){
+        document.querySelector(".weather-icon").src="images/haze.png";
+    }
+
+
+    document.querySelector(".weather").style.display="block";
+    
+}
