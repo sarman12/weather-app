abe69ce3e3f70d483ca8c7f9183764ad9c4ab62f
Sahanee Arman <115354835+sarman12@users.noreply.github.com>
2024-10-17 22:23:02 +0530

    gitignore

--- weather.js
+++ weather.js
@@ -1,12 +1,14 @@
-const apikey="65f7b612e3b7902c807bca207f229519";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const apikey = process.env.API_KEY;
 const apiurl="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const search=document.querySelector(".search input");
 const btn=document.querySelector(".search btn");
 
 async function checkWeather(city){
-    const response=await fetch(apiurl+city+`&appid=65f7b612e3b7902c807bca207f229519`);
+    const response=await fetch(apiurl+city+`&appid=`+apikey);
     var data=await response.json();
-    console.log(data);
     if(response.status == 404){
         document.querySelector(".error").style.display="block";
 
